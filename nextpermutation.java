bce4affdea6bcebd190f7bdf81525951e4f96416
Saijee Itokar <saijeeitokar509@gmail.com>
2026-09-25 12:45:23 +0530

    Add files via upload

---
+++ nextpermutation.java
@@ -0,0 +1,37 @@
+public class nextpermutation {
+    
+    class Solution {
+    public void nextPermutation(int[] nums) {
+
+        int pivot = -1;
+
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                pivot = i;
+                break;
+            }
+        }
+
+        for (int i = nums.length - 1; pivot != -1 && i > pivot; i--) {
+            if (nums[i] > nums[pivot]) {
+                int temp = nums[i];
+                nums[i] = nums[pivot];
+                nums[pivot] = temp;
+                break;
+            }
+        }
+
+        int start = pivot + 1;
+        int end = nums.length - 1;
+
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
+}
+}
